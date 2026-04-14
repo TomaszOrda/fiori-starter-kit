9d69f179d7ebb2e3815c7460ff3ff659ae77ba8d
Tomasz Orda <tomaszorda@outlook.com>
2026-04-14 13:26:36 +0000

    Implement contact info dialog inside customer details view

--- webapp/controller/CustomerDetails.controller.js
+++ webapp/controller/CustomerDetails.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "stk/starterkit/model/formatter"
-], (Controller, Formatter) => {
+    "stk/starterkit/model/formatter",
+    "sap/ui/core/Fragment"
+], (Controller, Formatter, Fragment) => {
     "use strict";
 
     return Controller.extend("stk.starterkit.controller.CustomerDetails", {
@@ -13,6 +14,35 @@ sap.ui.define([
                 //     expand: "Orders,Orders/Order_Details"
                 // }
             })
+        },
+        onShowContactInfo: function(){
+            var oView = this.getView();
+            if(!this.byId('contactDialog')){
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "stk.starterkit.view.ContactInfoDialog",
+                    //This allowes dialog to use onCloseContactInfo function
+                    controller: this
+                }).then(function (oDialog){
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+                
+                // The above is equivalent to the below, but the pressShowContactInfo function would have to be async
+                // From what i have read, .then registers somewhere an async function to be called
+                // Just as if we had a singleton that from time to time would call all the async functions that it gathered over time and then would simply send the function to this singleton
+
+                // const oDialog = await Fragment.load({..});
+                // oView.addDependent(oDialog);
+                // oDialog.open();
+            }else{
+                //The contactDialog gets killed only after the view is, 
+                // so we might have the same contactDialog handling different users
+                this.byId("contactDialog").open();
+            }
+        },
+        onCloseContactInfo : function (){
+            this.byId("contactDialog").close();
         },
         onInit() {
             const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
